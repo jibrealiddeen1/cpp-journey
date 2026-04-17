98b80ebdaf0545dedfc942e125e47c13dff8a9b6
Cold Worrell <coldworrell@Colds-MacBook-Air-3.local>
2026-04-16 23:45:01 -0400

    Updating/Practice/Understadning

--- Part 1/Part 2/Part 3/class.cpp
+++ Part 1/Part 2/Part 3/class.cpp
@@ -2,21 +2,44 @@
 #include <string>
 
 class Character {
-    public: 
+private: 
     std::string name;
     int health;
     int attackPower;
 
+public:
+
     Character(std::string n, int h, int ap){
-    name = n;
-    health = h;
-    attackPower = ap;
+        name = n;
+        health = h;
+        attackPower = ap;
     }
 
+    virtual void attack() = 0;
+
+
+    std::string getName(){ return name; }
+    int getHealth(){ return health; }
+    int getAttackPower(){ return attackPower; }
+
+    void setHealth(int h){
+        if(h > 0){
+            health = h;
+        }
+    }
+    
+    void setAttackPower(int ap){
+        if(ap > 0){
+            attackPower = ap;
+        }
+    }
+
+
+
     void displayinfo(){
-    std::cout << "===== " << name << " =====" << std::endl;
-    std::cout << "Health: " << health << std::endl;
-    std::cout << "Attack Power: " << attackPower << std::endl; 
+        std::cout << "===== " << name << " =====" << std::endl;
+        std::cout << "Health: " << health << std::endl;
+        std::cout << "Attack Power: " << attackPower << std::endl; 
     }
 
 };  
@@ -25,11 +48,13 @@ class Warrior : public Character {
     public:
         int shieldPower;
 
-        Warrior(std::string n, int h, int ap, int sp)
-            : Character(n,h,ap) {
+        Warrior(std::string n, int h, int ap, int sp) : Character(n,h,ap) {
                 shieldPower = sp; 
             }
 
+        void attack(){
+             std::cout << "Promise swings his sword!" << std::endl;
+        }
         void displayinfo(){
             Character::displayinfo();
             std::cout << "Shield Power: " << shieldPower << std::endl;
@@ -45,6 +70,10 @@ class Archer : public Character {
         Archer(std::string n, int h, int ap, int ard)
         : Character(n,h,ap) {
             arrowDamage = ard; 
+        }
+
+        void attack(){
+            std::cout << "E shoots an arrow!" << std::endl;
         }
 
          void displayinfo(){
@@ -64,6 +93,11 @@ class Mage : public Character {
             magicPower = mp;
         }
 
+         void attack(){
+            std::cout << "Cold casts a spell!" << std::endl;
+        }
+
+
          void displayinfo(){
             Character::displayinfo();
             std::cout << "Magic power: " << magicPower << std::endl;
@@ -72,13 +106,23 @@ class Mage : public Character {
 }; 
 
 int main(){
-    Warrior w1("Promise", 100, 75, 100);
-    Archer a1("E", 200, 500, 250);
-    Mage m1("Cold", 500, 650, 250);
+        Warrior w1("Promise", 100, 75, 100);
+        Archer a1("E", 200, 500, 250);
+        Mage m1("Cold", 500, 650, 250);
 
-    w1.displayinfo();
-    a1.displayinfo();
-    m1.displayinfo();
+        w1.setHealth(150);
+        w1.setAttackPower(-999);
+
+        // Test getter - read values
+        std::cout << "Updated Health: " << w1.getHealth() << std::endl;
+        std::cout << "Attack Power: " << w1.getAttackPower() << std::endl;
+
+        w1.displayinfo();
+        a1.displayinfo();
+        m1.displayinfo();
+        w1.attack();
+        a1.attack();
+        m1.attack();
     
     return 0;
 } 
